61cdc3a6fceb3e5c72ed8063d8c033ea5cae4a70
Ilona Rozenberg <ir2494@columbia.edu>
2026-06-25 22:03:35 -0400

    Connect frontend to FastAPI backend with CORS support

--- main.py
+++ main.py
@@ -1,7 +1,15 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 import requests
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root():
